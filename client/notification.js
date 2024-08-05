8895659831543d799d7818190d3da6c8588f7923
Vinay <vinaybhardwaj2334@gmail.com>
2024-08-05 22:04:58 +0530

    notification animation change

--- client/notification.js
+++ client/notification.js
@@ -19,18 +19,15 @@ function showNotification(message) {
         border-radius: 2px; /* Rounded borders */
         padding: 16px; /* Padding */
         position: fixed; /* Sit on top of the screen */
-        z-index: 1000; /* Add a z-index if needed */
+        z-index: 5000; /* Add a z-index if needed */
         left: 50%; /* Center the snackbar */
-        top: 30px; /* 30px from the top */
+        font-size: 15px;
       }
 
       /* Show the snackbar when clicking on a button (class added with JavaScript) */
       #snackbar.show {
         visibility: visible; /* Show the snackbar */
-        /* Add animation: Take 0.5 seconds to fade in and out the snackbar.
-        However, delay the fade out process for 2.5 seconds */
-        -webkit-animation: fadein 0.5s, fadeout 0.5s 0.5s;
-        animation: fadein 0.5s, fadeout 0.5s 2.5s;
+        animation: fadein 0.7s, fadeout 0.7s
       }
 
       /* Animations to fade the snackbar in and out */
@@ -66,7 +63,7 @@ function showNotification(message) {
   // Remove the 'show' class after the animation duration (3 seconds in total)
   setTimeout(() => {
     snackbar.className = snackbar.className.replace("show", "");
-  }, 3000); // 3 seconds for the full visibility including animation
+  }, 600); // 3 seconds for the full visibility including animation
 }
 
 // Expose the function to global scope for injection
